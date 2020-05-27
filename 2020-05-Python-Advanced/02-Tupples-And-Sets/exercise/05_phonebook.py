9ceb47ca8529a856be8f9232c47ec91c25e12fd9
Andrii Burka <andriiburka@gmail.com>
2020-05-27 04:32:09 +0300

    None

--- 2020-05-Python-Advanced/02-Tupples-And-Sets/exercise/05_phonebook.py
+++ 2020-05-Python-Advanced/02-Tupples-And-Sets/exercise/05_phonebook.py
@@ -1,15 +1,17 @@
-di = {}
+def solve(usr_inp):
+    contacts_di = {}
+    while not usr_inp.isdigit():
+        name, phone = usr_inp.split('-')
+        contacts_di[name] = phone
+        usr_inp = input()
+    usr_inp = int(usr_inp)
+    for _ in range(usr_inp):
+        current_name = input()
+        if current_name in contacts_di:
+            print(f"{current_name} -> {contacts_di[current_name]}")
+        else:
+            print(f"Contact {current_name} does not exist.")
 
-usr_inp = input()
-while not usr_inp.isdigit():
-    name, phone = [x for x in usr_inp.split('-')]
-    di[name] = phone
-    usr_inp = input()
 
-usr_inp = int(usr_inp)
-for i in range(usr_inp):
-    current_name = input()
-    if current_name in di:
-        print(f"{current_name} -> {di[current_name]}")
-    else:
-        print(f"Contact {current_name} does not exist.")
+if __name__ == '__main__':
+    solve(usr_inp=input())
