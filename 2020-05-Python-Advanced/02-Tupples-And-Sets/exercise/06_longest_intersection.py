0ed268bf5e4a4a89485dde57d1638fa8f32a299c
Andrii Burka <andriiburka@gmail.com>
2020-05-27 07:03:27 +0300

    None

---
+++ 2020-05-Python-Advanced/02-Tupples-And-Sets/exercise/06_longest_intersection.py
@@ -0,0 +1,18 @@
+def solve():
+    intersections = []
+    for _ in range(int(input())):
+        usr_inp = input().split('-')
+        li = []
+        for i1 in range(len(usr_inp)):
+            start, end = [start_end for start_end in list(map(int, usr_inp[i1].split(',')))]
+            tmp = []
+            [tmp.append(num) for num in range(start, end + 1)]
+            li.append(tmp)
+        intersections.append([intersection for intersection in set(li[0]).intersection(set(li[1]))])
+    lengths = [len(current_intersection) for current_intersection in intersections]
+    max_len_index = lengths.index(max(lengths))
+    print(f"Longest intersection is {intersections[max_len_index]} with length {len(intersections[max_len_index])}")
+
+
+if __name__ == '__main__':
+    solve()
