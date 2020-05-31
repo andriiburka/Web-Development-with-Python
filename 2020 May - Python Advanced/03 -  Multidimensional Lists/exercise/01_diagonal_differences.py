b152ead978bba2251c30cea38153de673809ba13
Andrii Burka <andriiburka@gmail.com>
2020-06-01 00:55:15 +0300

    None

--- 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/01_diagonal_differences.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/01_diagonal_differences.py
@@ -10,40 +10,40 @@ print(abs(sum(diagonal_decrease) - sum(diagonal_increase)))
 
 
 
-# v2
-matrix = [list(map(int, input().split())) for _ in range(int(input()))]
-diagonal1 = [(matrix[i][i]) for i in range(len(matrix))]
-diagonal2 = [(matrix[i][(len(matrix)-i-1)]) for i in range(len(matrix))]
-print(abs(sum(diagonal1) - sum(diagonal2)))
+# # v2
+# matrix = [list(map(int, input().split())) for _ in range(int(input()))]
+# diagonal1 = [(matrix[i][i]) for i in range(len(matrix))]
+# diagonal2 = [(matrix[i][(len(matrix)-i-1)]) for i in range(len(matrix))]
+# print(abs(sum(diagonal1) - sum(diagonal2)))
 
 
 
 
 
-# REGULAR SHOW
-n = int(input())
-matrix = list()
-for _ in range(n):
-    matrix.append(
-        list(map(int, input().split()))
-    )
-diagonal_started_top_left = list()
-for i in range(len(matrix)):
-    diagonal_started_top_left.append(
-        matrix[i][i]
-    )
-diagonal_started_top_right = list()
-list_1 = list()
-for h in range(len(matrix)):
-    list_1.append(h)
-list_2 = []
-for v in range(len(matrix) - 1, -1, -1):
-    list_2.append(v)
-for i in tuple(zip(list_1, list_2)):
-    vertical, horizontal = i[0], i[1]
-    diagonal_started_top_right.append(
-        matrix[vertical][horizontal]
-    )
-summed_top_left, summed_top_right = sum(diagonal_started_top_left), sum(diagonal_started_top_right)
-result = summed_top_left - summed_top_right
-print(abs(result))
+# # REGULAR SHOW
+# n = int(input())
+# matrix = list()
+# for _ in range(n):
+#     matrix.append(
+#         list(map(int, input().split()))
+#     )
+# diagonal_started_top_left = list()
+# for i in range(len(matrix)):
+#     diagonal_started_top_left.append(
+#         matrix[i][i]
+#     )
+# diagonal_started_top_right = list()
+# list_1 = list()
+# for h in range(len(matrix)):
+#     list_1.append(h)
+# list_2 = []
+# for v in range(len(matrix) - 1, -1, -1):
+#     list_2.append(v)
+# for i in tuple(zip(list_1, list_2)):
+#     vertical, horizontal = i[0], i[1]
+#     diagonal_started_top_right.append(
+#         matrix[vertical][horizontal]
+#     )
+# summed_top_left, summed_top_right = sum(diagonal_started_top_left), sum(diagonal_started_top_right)
+# result = summed_top_left - summed_top_right
+# print(abs(result))
--- 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/02_2x2_squares_in_matrix.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/02_2x2_squares_in_matrix.py
@@ -1,24 +1,14 @@
-# # 18:35
-#
-# rows, cols = list(map(int, input().split()))  # int, int
-# matrix = [input().split() for i in range(rows)]  # [str, str, str, str]
-# indices = list()
-# equals = 0
-#
-
-rows_count, cols_count = [int(x) for x in input().split()]
-
-matrix = []
-counter = 0
-for _ in range(rows_count):
-    line = [x for x in input().split()]
-    matrix.append(line)
-for row in range(rows_count - 1):
-    for col in range(cols_count - 1):
-        current = matrix[row][col]
-        current_right = matrix[row][col + 1]
-        bottom = matrix[row + 1][col]
-        bottom_right = matrix[row + 1][col + 1]
-        if current == current_right and current == bottom and current == bottom_right:
-            counter += 1
-print(counter)
+y, x = list(map(int, input().split()))
+matrix = list(list(map(str, input().split())) for i in range(y))
+matches, square_corners = 0, 4
+for row in range(y-1):
+    for column in range(x-1):
+        template = \
+            [matrix[row][column],       # C top-left-corner
+             matrix[row][column+1],     # D top-right-corner
+             matrix[row+1][column],     # A bottom-left-corner
+             matrix[row+1][column+1]]   # B bottom-right-corner
+        first_letter = template[0]
+        if list(first_letter * square_corners) == template:
+            matches += 1
+print(matches)
