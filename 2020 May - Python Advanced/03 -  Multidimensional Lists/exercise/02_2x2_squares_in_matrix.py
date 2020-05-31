5b8bc1aa914d5304a12fb0f1bd431a6e0182cf5a
Andrii Burka <andriiburka@gmail.com>
2020-05-31 21:08:27 +0300

    None

--- 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/02_2x2_squares_in_matrix.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/02_2x2_squares_in_matrix.py
@@ -1,18 +1,24 @@
-# 18:35
+# # 18:35
+#
+# rows, cols = list(map(int, input().split()))  # int, int
+# matrix = [input().split() for i in range(rows)]  # [str, str, str, str]
+# indices = list()
+# equals = 0
+#
 
-rows, cols = list(map(int, input().split()))  # int, int
-matrix = [input().split() for i in range(rows)]  # [str, str, str, str]
-indices = list()
-equals = 0
+rows_count, cols_count = [int(x) for x in input().split()]
 
-current_letter = ''
-for li in range(len(matrix)):
-    print(matrix[li])
-
-    for ch in matrix[li]:
-        if ch == current_letter:
-            indices.append(matrix[li][index(ch)])
-        else:
-            current_letter = ch
-
-    print()
+matrix = []
+counter = 0
+for _ in range(rows_count):
+    line = [x for x in input().split()]
+    matrix.append(line)
+for row in range(rows_count - 1):
+    for col in range(cols_count - 1):
+        current = matrix[row][col]
+        current_right = matrix[row][col + 1]
+        bottom = matrix[row + 1][col]
+        bottom_right = matrix[row + 1][col + 1]
+        if current == current_right and current == bottom and current == bottom_right:
+            counter += 1
+print(counter)
