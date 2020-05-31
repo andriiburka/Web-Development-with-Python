d5c004defbae8fe6bdec5d60b223a63a1a9a5e83
Andrii Burka <andriiburka@gmail.com>
2020-06-01 00:57:14 +0300

    None

--- 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/02_2x2_squares_in_matrix.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/exercise/02_2x2_squares_in_matrix.py
@@ -4,8 +4,8 @@ matches, square_corners = 0, 4
 for row in range(y-1):
     for column in range(x-1):
         template = \
-            [matrix[row][column],       # C top-left-corner
-             matrix[row][column+1],     # D top-right-corner
+            [matrix[row][column],       # D top-left-corner
+             matrix[row][column+1],     # C top-right-corner
              matrix[row+1][column],     # A bottom-left-corner
              matrix[row+1][column+1]]   # B bottom-right-corner
         first_letter = template[0]
