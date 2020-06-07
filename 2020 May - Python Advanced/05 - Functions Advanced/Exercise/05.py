56f39b7166075ea664520f61e8b37bca54c181a1
Andrii Burka <andriiburka@gmail.com>
2020-06-08 01:03:47 +0300

    None

--- 2020 May - Python Advanced/05 - Functions Advanced/Exercise/05.py
+++ 2020 May - Python Advanced/05 - Functions Advanced/Exercise/05.py
@@ -1,1 +1,17 @@
+def sum_numbers(num1, num2):
+    return num1 + num2
 
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+def func_executor(**kwargs):
+    print(kwargs)
+
+
+print(
+    func_executor(
+        (sum_numbers, (1, 2)), (multiply_numbers, (2, 4))
+    )
+)
