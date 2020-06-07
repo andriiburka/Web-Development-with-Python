f72a456b66bd5d865c48d1c1ef740d53e33a8fc0
Andrii Burka <andriiburka@gmail.com>
2020-06-08 00:22:46 +0300

    None

---
+++ 2020 May - Python Advanced/05 - Functions Advanced/Exercise/03_arguments_length.py
@@ -0,0 +1,8 @@
+def args_length(*args):
+    return len([i for i in args])
+
+
+# INPUTS
+print(args_length(1, 32, 5))            # 3
+print(args_length("john", "peter"))     # 2
+print(args_length([1, 2, 3]))           # 1
