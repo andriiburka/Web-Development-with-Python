c6c8814cca372c24429aaed8bb3898fe5dd37080
Andrii Burka <andriiburka@gmail.com>
2020-06-08 22:19:42 +0300

    None

--- 2020 May - Python Advanced/__________EXAM__________/PRACTICE/17-feb-2020/exam-advanced-17-feb-2020.sh
+++
@@ -1,7 +0,0 @@
-cd
-cd PycharmProjects/education
-git add .
-git commit -m "None"
-git push -u origin master
-cd
-open https://judge.softuni.bg/Contests/Practice/Index/2004#0
---
+++ 2020 May - Python Advanced/__________EXAM__________/PRACTICE/17-feb-2020/open_judge.sh
@@ -0,0 +1,1 @@
+open https://judge.softuni.bg/Contests/Practice/Index/2004#0
