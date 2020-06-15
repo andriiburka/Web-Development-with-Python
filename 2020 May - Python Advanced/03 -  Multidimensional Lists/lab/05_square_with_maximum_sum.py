93c5f598c16bd4e696fdf80b9d52c727f10ff341
Andrii Burka <andriiburka@gmail.com>
2020-06-15 20:36:33 +0300

    None

--- 2020 May - Python Advanced/03 -  Multidimensional Lists/lab/05_square_with_maximum_sum.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/lab/05_square_with_maximum_sum.py
@@ -1,14 +1,13 @@
 (row_count, col_count) = map(int, input().split(', '))
 mtx = [list(map(int, input().split(', '))) for _ in range(int(row_count))]
 
-row, col = 0, 0
-largest_sum = 0
+r, c, largest_sum = 0, 0, 0
 
-for r in range(row_count - 1):
-    for c in range(col_count - 1):
-        summed = sum([mtx[r][c], mtx[r][c + 1], mtx[r + 1][c], mtx[r + 1][c + 1]])
+for h in range(row_count - 1):
+    for v in range(col_count - 1):
+        summed = sum([mtx[h][v], mtx[h][v + 1], mtx[h + 1][v], mtx[h + 1][v + 1]])
         if summed > largest_sum:
-            row, col = r, c
+            r, c = h, v
             largest_sum = summed
 
-print('{} {}\n{} {}\n{}'.format(mtx[row][col], mtx[row][col + 1], mtx[row + 1][col], mtx[row + 1][col + 1], largest_sum))
+print('{} {}\n{} {}\n{}'.format(mtx[r][c], mtx[r][c + 1], mtx[r + 1][c], mtx[r + 1][c + 1], largest_sum))
