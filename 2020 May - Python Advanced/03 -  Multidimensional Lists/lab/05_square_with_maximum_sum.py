da6e908762ef9e944a95d24e38bbf927c28c67f7
Andrii Burka <andriiburka@gmail.com>
2020-06-15 20:14:16 +0300

    None

--- 2020 May - Python Advanced/03 -  Multidimensional Lists/lab/05_square_with_maximum_sum.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/lab/05_square_with_maximum_sum.py
@@ -1,3 +1,17 @@
-# НЕРЕШЕНА
-rows, cols = list(map(int, input().split(', ')))
-matrix = [list(map(int, input().split(', '))) for row in range(rows)]
+(row_count, col_count) = map(int, input().split(', '))
+matrix = [list(map(int, input().split(', '))) for _ in range(int(row_count))]
+
+row, col = 0, 0
+largest_sum = 0
+
+for r in range(row_count - 1):
+    for c in range(col_count - 1):
+
+        summed = sum([matrix[r][c], matrix[r][c + 1], matrix[r + 1][c], matrix[r + 1][c + 1]])
+
+        if summed > largest_sum:
+            row, col = r, c
+            largest_sum = summed
+
+print('{} {}\n{} {}'.format(matrix[row][col], matrix[row][col + 1], matrix[row + 1][col], matrix[row + 1][col + 1]))
+print(largest_sum)
