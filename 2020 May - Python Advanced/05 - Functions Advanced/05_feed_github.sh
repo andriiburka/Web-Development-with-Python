889b689623f6f6a6af89f608c281f4c98f47efb0
Andrii Burka <andriiburka@gmail.com>
2020-06-15 21:05:37 +0300

    None

--- 2020 May - Python Advanced/05 - Functions Advanced/05_feed_github.sh
+++ 2020 May - Python Advanced/05 - Functions Advanced/05_feed_github.sh
@@ -4,4 +4,3 @@ git add .
 git commit -m "None"
 git push -u origin master
 cd
-open https://github.com/andriiburka/education/tree/master/2020%20May%20-%20Python%20Advanced/05%20-%20Functions%20Advanced/Exercise
