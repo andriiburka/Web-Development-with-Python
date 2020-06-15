a8a4e34aafff25711bd678f0352eaca22a7e3fa6
Andrii Burka <andriiburka@gmail.com>
2020-06-16 01:38:54 +0300

    None

--- 2020 May - Python Advanced/03 -  Multidimensional Lists/lab/05_square_with_maximum_sum.py
+++ 2020 May - Python Advanced/03 -  Multidimensional Lists/lab/05_square_with_maximum_sum.py
@@ -3,19 +3,18 @@
 
 (row_count, col_count) = map(int, input().split(', '))
 mtx = [list(map(int, input().split(', '))) for _ in range(int(row_count))]
-output = tuple
+sub_matrix_winner = tuple
 largest_sum = 0
 
 for r in range(row_count - 1):
     for c in range(col_count - 1):
-        amount = (mtx[r][c],
-                  mtx[r][c + 1],
-                  mtx[r + 1][c],
-                  mtx[r + 1][c + 1])
+        sub_matrix_2x2 = [mtx[r][c],
+                          mtx[r][c + 1],
+                          mtx[r + 1][c],
+                          mtx[r + 1][c + 1]]
 
-        if amount > largest_sum:
-            output = amount
-            largest_sum = amount
+        if sum(sub_matrix_2x2) > largest_sum:
+            sub_matrix_winner = sub_matrix_2x2
+            largest_sum = sum(sub_matrix_2x2)
 
-print(output)
-# print('{} {}\n{} {}\n{}'.format(*output, largest_sum))
+print('{} {}\n{} {}\n{}'.format(*sub_matrix_winner, largest_sum))
