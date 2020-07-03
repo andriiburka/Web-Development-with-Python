e9145f9095d10801d87428a45bcfe2c7ea122bf6
Andrii Burka <andriiburka@gmail.com>
2020-07-03 15:39:52 +0300

    Uploaded 05_programmer.py (OOP)

---
+++ 2020.06 - Python OOP/01_Defining_Classes/exercise/05_programmer.py
@@ -0,0 +1,28 @@
+class Programmer:
+    def __init__(self, name: str, language: str, skills: int):
+        self.name, self.language, self.skills = name, language, skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills >= skills_needed and new_language != self.language:
+            prv_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {prv_language} to {new_language}"
+        elif self.skills >= skills_needed and new_language == self.language:
+            return f"{self.name} already knows {new_language}"
+        elif new_language != self.language and self.skills < skills_needed:
+            return f"{self.name} needs {skills_needed - self.skills} more skills"
+
+
+programmer = Programmer("John", "Java", 50)  # knows
+print(programmer.watch_course("Python Masterclass", "Python", 84))  # John does not know Python
+print(programmer.change_language("Java", 30))  # John already knows Java ()
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Java: zero to hero", "Java", 50))
+print(programmer.change_language("Python", 100))
+print(programmer.watch_course("Python Masterclass", "Python", 84))
--- RUNME.sh
+++ RUNME.sh
@@ -1,6 +1,6 @@
 cd
 cd PycharmProjects/education
 git add .
-git commit -m "Uploaded Steam_User.py (OOP)"
+git commit -m "Uploaded 05_programmer.py (OOP)"
 git push -u origin master
 cd
