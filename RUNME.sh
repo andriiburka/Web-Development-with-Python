c4e5b20f315fe485175bfa4b60fe5df71b7e7550
Andrii Burka <andriiburka@gmail.com>
2020-07-08 20:16:54 +0300

    sh

--- RUNME.sh
+++ RUNME.sh
@@ -1,9 +1,8 @@
 cd
 cd PycharmProjects/education
+git config --global user.email "andriiburka@gmail.com"
 git add .
 read msg
 git commit -m $msg
 git push -u origin master
 cd
-git config --global user.email "andriiburka@gmail.com"
-
