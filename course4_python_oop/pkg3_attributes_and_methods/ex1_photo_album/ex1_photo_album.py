b64dbe57b6693b1215c336cd4f0a11564fea5dc8
andriiburka <andriiburka@gmail.com>
2020-07-10 06:21:16 +0300

    .format

--- course4_python_oop/pkg3_attributes_and_methods/ex1_photo_album/ex1_photo_album.py
+++ course4_python_oop/pkg3_attributes_and_methods/ex1_photo_album/ex1_photo_album.py
@@ -4,29 +4,26 @@ class PhotoAlbum:
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        """New instance maker"""
         return cls(photos_count // 4)
 
     def add_photo(self, label):
         for i in range(len(self.photos)):
-
             if len(self.photos[i]) < 4:
                 self.photos[i].append(label)
                 return "{} photo added successfully on page {} slot {}"\
                     .format(label, i + 1, len(self.photos[i]))
-
         return "No more free spots"
 
     def display(self):
         result = '{:->11}\n'.format('')
         for page in self.photos:
             if page:
-                result += ''.join('[] ' for _ in range(len(page)))[:-1]
+                result += ' '.join('[]' for _ in range(len(page)))
             result += '\n{:->11}\n'.format('')
         return result
 
 
-album = PhotoAlbum(1)
+album = PhotoAlbum(4)
 
 print(album.add_photo("baby"))
 print(album.add_photo("first grade"))
